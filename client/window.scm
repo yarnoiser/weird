1c0aafbb6d75eaf72b597043ae61ef1abb92177b
Robert Smiley <yarnoiser@gmail.com>
2016-10-26 16:35:40 -0600

    more drawing functions

--- client/window.scm
+++ client/window.scm
@@ -8,6 +8,9 @@
 
 (define window #f)
 
+(define (window-needs-update?)
+  (not (null? dirty-rects)))
+
 (define (window-init! title width height)
   (sdl2:set-main-ready!)
   (sdl2:init! '(video))
@@ -23,10 +26,17 @@
 
 (define window-quit-requested? sdl2:quit-requested?)
 
-(define (window-fill! color)
-  (sdl2:fill-rect! (sdl2:window-surface window) #f color))
+(define (window-mark-dirty!)
+  (list-push! dirty-rects (rect 0 0 (sld2:surface-w (sdl2:window-surface))
+                                    (sdl2:surface-h (sdl2:window-surface)))))
 
-(define window-update! sdl2:update-window-surface!)
+(define (window-fill! color)
+  (sdl2:fill-rect! (sdl2:window-surface window) #f color)
+  (window-mark-dirty!))
+
+(define (window-update!)
+  (apply sdl2:update-window-surface-rects (list window dirty-rects))
+  (set! dirty-rects '()))
 
 (define image-load sdl2:load-bmp)
 
@@ -35,9 +45,10 @@
 (define point sdl2:make-point)
 
 (define (image-crop-draw! image source-rect  dest-rect)
-  (sdl2:blit-surface! image source-rect (sdl2:window-surface window) dest-rect))
+  (sdl2:blit-surface! image source-rect (sdl2:window-surface window) dest-rect)
+  (list-push! dirty-rects dest-rect))
 
 (define (image-draw! image x y)
-  (image-crop-draw! image (rect 0 0 (surface-w image) (surface h image))
-                          (rect x y (surface-w image) (surface h image))))
+  (image-crop-draw! image (rect 0 0 (sdl2:surface-w image) (sdl2:surface h image))
+                          (rect x y (sdl2:surface-w image) (sdl2:surface h image))))
 
