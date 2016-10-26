3d58c2d8757d49e8e21df60f7d95764ee04f8d58
Robert Smiley <yarnoiser@gmail.com>
2016-10-26 11:57:43 -0600

    Untested image drawing

---
+++ client/gtk.scm
@@ -0,0 +1,7 @@
+(foreign-declare "#include <gtk/gtk.h>")
+
+(define-foreign-type gpointer c-pointer)
+(define-foreign-type gtk-application c-pointer)
+(define-foreign-type gtk-widget c-pointer)
+
+(define 
--- client/window.scm
+++ client/window.scm
@@ -1,5 +1,8 @@
-(declare (unit window))
+(declare (unit window) (uses debug))
 (use (prefix sdl2 sdl2:))
+(include "shared/macros.scm")
+
+(define dirty-rects '())
 
 (define black (sdl2:make-color 0 0 0))
 
@@ -20,12 +23,21 @@
 
 (define window-quit-requested? sdl2:quit-requested?)
 
-(define (window-draw-blank!)
-  (sdl2:fill-rect! (sdl2:window-surface window) #f black))
+(define (window-fill! color)
+  (sdl2:fill-rect! (sdl2:window-surface window) #f color))
 
-(define (window-update!)
-  (sdl2:update-window-surface! window))
+(define window-update! sdl2:update-window-surface!)
 
 (define image-load sdl2:load-bmp)
 
+(define rect sdl2:make-rect)
+
+(define point sdl2:make-point)
+
+(define (image-crop-draw! image source-rect  dest-rect)
+  (sdl2:blit-surface! image source-rect (sdl2:window-surface window) dest-rect))
+
+(define (image-draw! image x y)
+  (image-crop-draw! image (rect 0 0 (surface-w image) (surface h image))
+                          (rect x y (surface-w image) (surface h image))))
 
