f8ed15b3785e64367f5706b65cee7cdc53d3c128
Robert Smiley <yarnoiserdev@gmail.com>
2016-10-26 22:05:01 -0600

    test sprites

--- client/gtk.scm
+++
@@ -1,7 +0,0 @@
-(foreign-declare "#include <gtk/gtk.h>")
-
-(define-foreign-type gpointer c-pointer)
-(define-foreign-type gtk-application c-pointer)
-(define-foreign-type gtk-widget c-pointer)
-
-(define 
