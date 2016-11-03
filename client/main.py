36c6569b5763eab54a9f0f4705f308ecbc0ab166
Robert Smiley <yarnoiser@gmail.com>
2016-11-03 11:24:58 -0600

    Testing Image class

--- client/main.py
+++ client/main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import window
+import ctypes
 
 window.init()
 
 win = window.new("weird client", 640, 480)
 
-monster = [window.loadImage("client/data/sprites/monster1/monster1-1.png"),
-           window.loadImage("client/data/sprites/monster1/monster1-2.png")]
+monster = [window.loadImage("client/data/sprites/monster1/monster1-1.bmp"),
+           window.loadImage("client/data/sprites/monster1/monster1-2.bmp")]
 
 win.drawImage(monster[0], 10, 10)
 
--- client/window.py
+++ client/window.py
@@ -4,33 +4,54 @@ import ctypes
 import weakref
 import atexit
 
+class Image:
+  def __init__(self, path):
+    self.surfacePtr = IMG_Load(path.encode("utf-8"))
+
+    if not self.surfacePtr:
+      raise FileNotFoundError(path)
+
+    weakref.finalize(self.surfacePtr, SDL_FreeSurface, self.surfacePtr)
+
+  def width(self):
+    return self.surfacePtr.contents.w
+
+  def height(self):
+    return self.surfacePtr.contents.h
+
 class Window:
   def __init__(self, title, width, height):
     self.win = SDL_CreateWindow(title.encode('utf-8'), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                          width, height, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE)
     weakref.finalize(self.win, SDL_DestroyWindow, self.win)
-    self.surface = SDL_GetWindowSurface(self.win)
+
+  def surface(self):
+    SDL_GetWindowSurface(self.win)
 
   def drawCroppedImage(self, image, srcRect, destRect):
-    SDL_BlitSurface(image, srcRect, self.surface, destRect)
+    print(image.surfacePtr)
+    print(srcRect)
+    print(self.surface())
+    print(destRect)
+    SDL_BlitSurface(image.surfacePtr, srcRect, self.surface(), destRect)
 
   def drawImage(self, image, x, y):
-    self.drawCroppedImage(image, None, rect(x, y, image.w, image.h))
+    self.drawCroppedImage(image, None, rect(x, y, image.width(), image.height()))
 
-  def update():
-    SDL_updateWindowSurface(self.win)
+  def update(self):
+    SDL_UpdateWindowSurface(self.win)
 
 def init():
   SDL_Init(SDL_INIT_VIDEO)
   atexit.register(SDL_Quit)
+  IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF)
+  atexit.register(IMG_Quit)
 
 def new(title, width, height):
   return Window(title, width, height)
 
 def loadImage(path):
-  img = IMG_Load(path.encode("utf-8"))
-  weakref.finalize(img, SDL_FreeSurface, img)
-  return img
+  return Image(path)
 
 def point(x, y):
   SDL_Point(x, y)
