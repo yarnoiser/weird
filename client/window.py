e379121161eeca1c2bf4040778dcdeb9752c6818
Robert Smiley <yarnoiser@gmail.com>
2016-11-03 11:46:20 -0600

    Image drawing works

--- client/window.py
+++ client/window.py
@@ -26,13 +26,9 @@ class Window:
     weakref.finalize(self.win, SDL_DestroyWindow, self.win)
 
   def surface(self):
-    SDL_GetWindowSurface(self.win)
+    return SDL_GetWindowSurface(self.win)
 
   def drawCroppedImage(self, image, srcRect, destRect):
-    print(image.surfacePtr)
-    print(srcRect)
-    print(self.surface())
-    print(destRect)
     SDL_BlitSurface(image.surfacePtr, srcRect, self.surface(), destRect)
 
   def drawImage(self, image, x, y):
@@ -54,8 +50,8 @@ def loadImage(path):
   return Image(path)
 
 def point(x, y):
-  SDL_Point(x, y)
+  return SDL_Point(x, y)
 
 def rect(x, y, w, h):
-  SDL_Rect(x, y, w, h)
+  return SDL_Rect(x, y, w, h)
 
